2b9f0809951453e86a03d0cf92a07cf78a8f46d0
김경민 <gminkim@gimgyeongmin-ui-iMac.local>
2021-01-22 00:41:37 +0900

    create router

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ import morgan from "morgan"
 import helmet from "helmet"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
-
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
+import globalRouter from "./routers/globalRouter"
 
 const app = express()
-const handleProfile  = (req,res)=>{res.send("you are on my Profile")}
-const handleHome  = (req,res)=>{res.send("hello from home")}
-const middleware = (req, res, next)=>{
-  res.send("not happening")
-}
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -20,7 +17,7 @@ app.use(helmet())
 app.use(morgan(
   "dev"
 ))
-app.get("/", middleware, handleHome)
-app.get("/profile", handleProfile)
-
+app.use("/", globalRouter)
+app.use("/user", userRouter)
+app.use("/video", videoRouter)
 export default app;
--- init.js
+++ init.js
@@ -1,5 +1,4 @@
 import app from "./app"
 const PORT = 4000;
-
 const handleListening = () =>{console.log("📢 Listening on : http://localhost:"+PORT)}
 app.listen(PORT, handleListening) 
